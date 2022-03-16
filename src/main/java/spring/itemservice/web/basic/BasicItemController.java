425e2067858a64d9b0ed67aa8abd9def5b688782
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-16 23:54:42 +0900

    RedirectAttributes

--- src/main/java/spring/itemservice/web/basic/BasicItemController.java
+++ src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spring.itemservice.domain.item.Item;
 import spring.itemservice.domain.item.ItemRepository;
 
@@ -91,10 +92,24 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    /**
+     * PRG - Post/Redirect/Get
+     */
+    //@PostMapping("/add")
     public String addItemV5(Item item){
-        itemRepository.save(item);
-        return "redirect:/basic/items/"+item.getId();
+        Item savedItem = itemRepository.save(item);
+        return "redirect:/basic/items/"+savedItem.getId();
+    }
+
+    /**
+     * RedirectAttributes
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
